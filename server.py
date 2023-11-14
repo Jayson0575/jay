9f52f9dfe2ee62e52765f9c18f8aeac49b85ef75
Jayson0575 <150652152+Jayson0575@users.noreply.github.com>
2023-11-14 15:40:07 +0800

    Update server.py

--- server.py
+++ server.py
@@ -35,5 +35,6 @@ def get_weather():
     )
 
 
-if __name__ == "__main__":
-    serve(app, host="0.0.0.0", port=8000)
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=8080)
+    app.run(debug=True)
